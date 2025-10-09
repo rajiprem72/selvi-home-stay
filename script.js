b3adb983e2846d8fa251c70c6c3a68bf3c6cb85c
Premanandhan Narayanan <premanandhan@gmail.com>
2025-10-09 19:34:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const toggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector("nav");
+  if (toggle) toggle.addEventListener("click", () => nav.classList.toggle("active"));
+});
